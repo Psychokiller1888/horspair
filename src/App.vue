3a77084d4b10dd5fcef62afa9abb4d9b4f98169c
Psychokiller1888 <laurentchervet@bluewin.ch>
2022-02-11 05:30:26 +0100

    :sparkles: backup

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,15 @@ export default {
 		Footer
   },
 	created() {
+		this.$store.dispatch('init')
+		this.$store.dispatch(
+			'insert',
+			{
+				'table': 'moodTracking',
+				'timestamp': Date.now(),
+				'mood': '-1'
+			}
+		)
 	}
 }
 </script>
