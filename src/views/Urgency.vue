edb7c48ace73b5566a7132ad31afcc4afe6d1759
Psychokiller1888 <laurentchervet@bluewin.ch>
2022-03-27 18:55:47 +0200

    urgencies

--- src/views/Urgency.vue
+++ src/views/Urgency.vue
@@ -2,35 +2,64 @@
 	<div class="mainContainer">
 		<div class="pageContent">
 			<p class="title">urgences</p>
-			<p class="explanations">
-				Tu as besoin d'aide? Besoin de parler, besoin d'écoute? Besoin de te changer les idées, de soutient ou simplement d'une main tendue? Les gens qui te répondront ici ne sont pas des professionnels, mais des gens comme toi, inscris à notre programme "Anges Gardien", offrant de leur personne en cas de besoin.
-			</p>
-			<p class="explanations explanation">
-				En utilisant ce service, les anges gardiens seront avertis et le premier à répondre recevra ton numéro de téléphone pour pouvoir te contacter. Il se peut que personne ne soit disponible au moment où tu en as besoin, auquel cas nous te conseillons, si ça ne va vraiment pas, de contacter les instances officielles.
-			</p>
-			<div class="helpButton" v-if="timer <= 0">
-				<div class="helpButtonInner">
-					help
+			<div v-if="timer <= 0">
+				<p class="explanations">
+					Tu as besoin d'aide? Besoin de parler, besoin d'écoute? Besoin de te changer les idées, de soutient ou simplement d'une main tendue? Les gens qui te répondront ici ne sont pas des professionnels, mais des gens comme toi, inscris à notre programme "Anges Gardien", offrant de leur personne en cas de besoin.
+				</p>
+				<p class="explanations explanation">
+					En utilisant ce service, les anges gardiens seront avertis et le premier à répondre recevra ton numéro de téléphone pour pouvoir te contacter. Il se peut que personne ne soit disponible au moment où tu en as besoin, auquel cas nous te conseillons, si ça ne va vraiment pas, de contacter les instances officielles.
+				</p>
+				<div class="helpButton" v-if="timer <= 0" @click="askForHelp">
+					<div class="helpButtonInner">
+						help
+					</div>
 				</div>
 			</div>
-			<p class="countdown" v-if="timer > 0">
-				00:00
-			</p>
+			<div v-else>
+				<p class="explanations">
+					Ta demande a été transmise et les anges gardien disponible maintenant ont été alerté
+				</p>
+				<p class="countdown">
+					{{ remainingMinutes }}:{{ remainingSeconds }}
+				</p>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
 
+import commons from '@/js/commons';
+
 export default {
 	name: 'Urgency',
 	data: function() {
 		return {
-			timer: 10
+			timer: 0,
+			remainingMinutes: '00',
+			remainingSeconds: '00'
 		}
 	},
 	methods: {
+		askForHelp: function() {
+			this.$store.state.axios.get('/guardianAngel/urgency/').then(response => {
+				this.timer = 300
+				this.countdown()
+			}).catch(() => {
+				this.timer = 300
+				this.countdown()
+			})
+		},
+		countdown: function() {
+			if (this.timer <= 0) {
+				return
+			}
 
+			this.remainingMinutes = commons.addZeroBefore(Math.floor(this.timer / 60 ).toString())
+			this.remainingSeconds = commons.addZeroBefore(Math.floor(this.timer % 60 ).toString())
+			this.timer--
+			setTimeout(this.countdown, 1000)
+		}
 	}
 }
 </script>
@@ -79,11 +108,11 @@ export default {
 
 
 .helpButtonInner:hover {
-	background-color: #dc0000;
+	filter: brightness(85%);
 }
 
 .helpButtonInner:active {
-	background-color: #3d0000;
+	filter: brightness(50%);
 }
 
 .countdown {
