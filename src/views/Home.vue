23c6a1bd814c9cb4f61715ca0c7c145b0879a84a
Psychokiller1888 <laurentchervet@bluewin.ch>
2022-07-07 22:09:30 +0200

    Français

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
 	<div class="mainContainer">
 		<div class="homepage">
+<!--						<div class="box center">-->
+<!--							<div class="boxTitle">-->
+<!--								comprendre décider assumer-->
+<!--							</div>-->
+<!--							<div class="boxContent">-->
+<!--								<p>-->
+<!--									Par manque d'intérêt et de communauté, ce projet est annulé.-->
+<!--								</p>-->
+<!--							</div>-->
+<!--						</div>-->
 			<div class="box left">
 				<div class="boxTitle">
 					Surdoué haut potentiel zèbre intello HP HPI HPE sur-efficient
@@ -10,7 +20,7 @@
 						Tu as sûrement déjà entendu ces termes! Tu te sens différent(e)? Ca tombe bien, nous aussi!
 					</p>
 					<p>
-						Tu te sens en décalage avec ton entourage? Tu t'ennuies alors que tes amis s'éclatent? Tes emotions te dépassent? Tu es bien plus rapide que les autres dans les calculs ou la jugeote? Tu sens les choses venir et tu es capable de finir les phrases de tes proches avant même le milieu de leur phrase? Ou alors depuis toujours tu t'es ressentis rejeté(e) ou mis(e) à l'écart et tu as du te forcer pour pouvoir t'intégrer?
+						Tu te sens en décalage avec ton entourage? Tu t'ennuies alors que tes amis s'éclatent? Tes emotions te dépassent? Tu es bien plus rapide que les autres dans les calculs ou la jugeote? Tu sens les choses venir et tu es capable de finir les phrases de tes proches avant même le milieu de leur phrase? Ou alors depuis toujours tu te ressens rejeté(e) ou mis(e) à l'écart et tu as du te forcer pour pouvoir t'intégrer?
 					</p>
 					<p>
 						La médisance, l'injustice, les inégalités, la méchanceté gratuite t'indignent?
@@ -33,7 +43,7 @@
 				</div>
 				<div class="boxContent">
 					<p>
-						Non, tu ne dois pas forcement tous les cocher et au contraire des croyances archaïques promues autant par certains groupes élitistes ainsi que la médiatisation de notre prédisposition mentale, tu n'es pas obligé(e) d'avoir un QI de plus de 130, ni de pouvoir terminer un Rubik's Cube en moins de 10 secondes les yeux bandés et les mains attachées dans le dos pour être un(e) HP! Ca peut aider hein, mais ce ne sont pas des conditions sine qua non.
+						Non, tu ne dois pas forcement tous les cocher et au contraire des croyances archaïques, promues autant par certains groupes élitistes ainsi que la médiatisation de notre prédisposition mentale, tu n'es pas obligé(e) d'avoir un QI de plus de 130, ni de pouvoir terminer un Rubik's Cube en moins de 10 secondes les yeux bandés et les mains attachées dans le dos pour être un(e) HP! Ca peut aider hein, mais ce ne sont pas des conditions sine qua non.
 					</p>
 				</div>
 			</div>
@@ -43,7 +53,7 @@
 				</div>
 				<div class="boxContent">
 					<p>
-						La seule chose qui est différente chez toi comme chez nous, c'est que tu n'utilises pas ton cerveaux comme la majorité de la population et, crois-nous, ça te donnes un certain avantage, même si tu penses que c'est un problème. Et nous sommes ici pour t'accueillir, t'accompagner et t'aider, sans jugement mais aussi pour apprendre de toi, tout simplement.
+						La seule chose qui est différente chez toi comme chez nous, c'est que tu n'utilises pas ton cerveau comme la majorité de la population et, crois-nous, ça te donnes un certain avantage, même si tu penses que c'est un problème. Et nous sommes ici pour t'accueillir, t'accompagner et t'aider, sans jugement mais aussi pour apprendre de toi, tout simplement.
 					</p>
 					<div class="button" @click="joinModal = true" v-if="!$store.state.user">Rejoindre la communauté</div>
 				</div>
