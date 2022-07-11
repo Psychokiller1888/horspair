fe27a39f38a0950c1b116f84792c689e899e6d37
Psychokiller1888 <laurentchervet@bluewin.ch>
2022-07-11 20:15:02 +0200

    Concept anti procrastination part 1

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,5 +1,28 @@
 <template>
-	<div class="mainContainer">notes</div>
+	<div class="mainContainer">
+		<div class="board">
+			<div class="postit" style="background-color: #488a4b; top: 135px; left: 632px; transform: rotate(12deg)">
+				<div class="note">
+					Terminer le design concept
+					Terminer le design concept
+					Terminer le design concept
+					Terminer le design concept
+					Terminer le design concept
+					Terminer le design concept
+					Terminer le design concept
+					Terminer le design concept
+					Terminer le design concept
+				</div>
+				<div class="deadline">15 juillet 2022, 17h30</div>
+			</div>
+			<div class="postit" style="background-color: #93ff96; top: 182px; left: 836px; transform: rotate(-7deg); color: #1e1e1e;">
+				<div class="note">
+					<strong>Appeler les Toises</strong>
+				</div>
+				<div class="deadline red">02 juillet 2022, 12h00</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -9,5 +32,41 @@ export default {
 </script>
 
 <style scoped>
+	.board {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		background-color: #04000c;
+		flex-grow: 1;
+		position: relative;
+	}
 
+	.postit {
+		position: absolute;
+		width: 200px;
+		min-height: 200px;
+		-webkit-box-shadow: 4px 8px 15px 3px #000000;
+		box-shadow: 4px 8px 15px 3px #000000;
+		padding: 10px;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.note {
+		font-family: "Comic Sans MS", "Monotype Corsiva", "Bradley Hand ITC", serif;
+		flex-grow: 1;
+		text-align: justify;
+	}
+
+	.deadline {
+		margin-top: 10px;
+		font-size: 0.8em;
+		text-align: right;
+	}
+
+	.red {
+		color: red;
+		font-weight: bolder;
+	}
 </style>
